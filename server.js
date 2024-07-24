d98e76d190284bb8ab75714fb9fa8f4caa63235f
codingwithtim73 <91760043+codingwithtim73@users.noreply.github.com>
2024-07-24 14:39:44 +1000

    fixed production server issues

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import { createRequestHandler } from "@remix-run/express";
 import express from "express";
+import dotenv from "dotenv";
+import compression from "compression"; // Import compression middleware
+import morgan from "morgan";
+// Load environment variables from .env file
+dotenv.config();
 
 const port = process.env.PORT || 3000;
+const environment = process.env.NODE_ENV || "development";
+console.log("Environment:", environment);
+console.log("The port is", port);
 
 const viteDevServer =
-  process.env.NODE_ENV === "production"
+  environment === "production"
     ? null
     : await import("vite").then((vite) =>
         vite.createServer({
@@ -12,17 +20,37 @@ const viteDevServer =
         })
       );
 
+const remixHandler = createRequestHandler({
+  build: viteDevServer
+    ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
+    : await import("./build/server/index.js"),
+});
 const app = express();
-app.use(
-  viteDevServer ? viteDevServer.middlewares : express.static("build/client")
-);
 
-const build = viteDevServer
-  ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
-  : await import("./build/server/index.js");
+// Use compression middleware
+app.use(compression());
 
-app.all("*", createRequestHandler({ build }));
+// http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
+app.disable("x-powered-by");
+
+if (viteDevServer) {
+  app.use(viteDevServer.middlewares);
+} else {
+  // Vite fingerprints its assets so we can cache forever.
+  app.use(
+    "/assets",
+    express.static("build/client/assets", { immutable: true, maxAge: "1y" })
+  );
+}
+
+// Everything else (like favicon.ico) is cached for an hour. You may want to be
+// more aggressive with this caching.
+app.use(express.static("build/client", { maxAge: "1h" }));
+app.use(morgan("tiny"));
+
+// handle SSR requests
+app.all("*", remixHandler);
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`Server started in ${environment} mode on port ${port}`);
 });
--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+  ],
 });
